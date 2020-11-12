4396a1f326c9a1b9ef7ef22bf7f272da50a28c90
SamGarner <samuel.w.garner@gmail.com>
2020-11-12 11:41:11 -0600

    init connect four TDD

---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: false
+
+# test that an empty game board is created
+# test that piece falls to lowest unfilled point in the column
+# test same but next turn is other players' piece

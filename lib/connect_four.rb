d39e3eaece981a7f8e89431f6fa23b18fbc93865
SamGarner <samuel.w.garner@gmail.com>
2020-11-12 13:51:44 -0600

    Game init and associated tests

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,9 +1,39 @@
 # frozen_string_literal: false
 
+class Game
+  attr_reader :player1, :player2
+  attr_accessor :game_board, :next_move
+
+  def initialize
+    @player1 = '☭'
+    @player2 = '₴'
+    @next_move = [1, 2].sample
+    @game_board = GameBoard.new
+  end
+end
+
 class GameBoard
   attr_accessor :board_array
 
   def initialize
     @board_array = Array.new (6) { Array.new (7) }
+  end
+
+  def add_piece(player = 1, column)
+    if board_array[5][column].nil?
+      board_array[5][column] = player
+    elsif board_array[4][column].nil?
+      board_array[4][column] = player
+    elsif board_array[3][column].nil?
+      board_array[3][column] = player
+    elsif board_array[2][column].nil?
+      board_array[2][column] = player
+    elsif board_array[1][column].nil?
+      board_array[1][column] = player
+    elsif board_array[1][column].nil?
+      board_array[1][column] = player
+    else 
+      puts 'That column is already full - choose another column to place your piece.'
+    end
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -7,6 +7,39 @@ require_all 'lib/'
 # test that piece falls to lowest unfilled point in the column
 # test same but next turn is other players' piece
 
+describe Game do
+  describe '#initialize' do
+    it 'player 1 initialized' do
+      game = Game.new
+
+      expect(game.player1).not_to be_nil
+    end
+
+    it 'player 2 initialized' do
+      game = Game.new
+
+      expect(game.player2).not_to be_nil
+    end
+
+    it 'player 1 and player 2 are separately identifiable' do
+      game = Game.new
+
+      expect(game.player1).not_to eq(game.player2)
+    end
+
+    it 'next_move is set to one of the two players' do
+      game = Game.new
+
+      expect(game.next_move).to (satisfy { |n| [1,2].include?(n) })
+    end
+
+    it 'one GameBoard is initialized' do 
+      game = Game.new
+      expect(game.game_board).to be_truthy
+    end
+  end
+end
+
 describe GameBoard do
   describe '#initialize' do
     it 'a new gameboard will contain 6 elements' do
@@ -18,13 +51,16 @@ describe GameBoard do
     it 'each of the 6 elements will be an array of 7' do
       board = GameBoard.new
 
-      expect(board.board_array).to all(eq([nil, nil, nil, nil, nil, nil, nil]))
+      expect(board.board_array).to all(eq([nil, nil, nil, nil, nil, nil, nil]))  #########
     end
   end
 
   describe '#add_piece' do
     it 'adds piece to first row when player 1 goes first' do
       board = GameBoard.new
+      board.add_piece(1, 3)
+
+      expect(board.board_array[5][3]).to eq(1)
     end
   end
 end

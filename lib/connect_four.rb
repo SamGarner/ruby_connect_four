33949c20eb4c90c2b866af627120f459d11b290e
SamGarner <samuel.w.garner@gmail.com>
2020-11-13 12:40:30 -0600

    working game, next handle invalid input and player piece display better

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -4,17 +4,41 @@ require 'pry'
 
 class Game
   attr_reader :player1, :player2
-  attr_accessor :game_board, :next_move
+  attr_accessor :game_board, :next_move, :game_over, :play_column
 
   def initialize
     @player1 = '☭'
     @player2 = '₴'
     @next_move = [1, 2].sample
     @game_board = GameBoard.new
+    @game_over = 0
   end
 
   def update_which_player_has_next_move(next_play = next_move)
     next_play == 1 ? self.next_move = 2 : self.next_move = 1
+  end
+
+  def get_user_next_play
+    puts "Enter an integer 0 through 6 for which column you'd like to play:"
+    @play_column = gets.chomp.to_i
+  end
+
+  def play
+    puts "Welcome to connect four! Player#{next_move} goes first."
+    while game_over.zero? 
+      get_user_next_play
+      next if game_board.add_piece(next_move, play_column) == false
+      game_board.display_board
+      self.game_over = 1 if game_board.check_for_win
+      self.game_over = 2 if game_board.check_for_draw
+      break if game_over > 0
+
+      # game_over = 1 if check_for_win || check_for_draw
+      update_which_player_has_next_move
+    end
+    puts "Player#{next_move} wins!" if game_over == 1
+    puts "It's a draw!" if game_over == 2
+    # exit
   end
 end
 
@@ -38,13 +62,27 @@ class GameBoard
       board_array[1][column] = player
     elsif board_array[1][column] == '-'
       board_array[1][column] = player
+    elsif board_array[0][column] == '-'
+      board_array[0][column] = player
     else 
       puts 'That column is already full - choose another column to place your piece.'
+      false
     end
   end
 
+  def display_board # wait on final piece display to format/even out, # puts board array separates values
+    print "#{board_array[0]} \n"
+    print "#{board_array[1]} \n"
+    print "#{board_array[2]} \n"
+    print "#{board_array[3]} \n"
+    print "#{board_array[4]} \n"
+    print "#{board_array[5]} \n"
+    print "  0    1    2    3    4    5    6 \n"
+  end
+
   def check_for_draw
-    board_array.all? { |n| n.compact.length == 7 }
+    # board_array.all? { |n| n.compact.length == 7 }
+    !board_array[0].include?('-')
   end
 
   def check_for_win
@@ -111,3 +149,7 @@ class GameBoard
     false
   end
 end
+
+
+game = Game.new
+game.play
